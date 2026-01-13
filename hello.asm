d94c7dd10b83e48d01190bceeeec9141fcf30416
Lucas Alves <olalvezluc@gmail.com>
2026-01-12 23:24:31 -0300

    Sobe hello world com fasm (flatassembler)

---
+++ hello.asm
@@ -0,0 +1,18 @@
+format ELF64 executable
+
+segment readable executable
+entry main
+
+main: 
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 14
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+segment readable writeable
+msg db "Hello, World!", 10
