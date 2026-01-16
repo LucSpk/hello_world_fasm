d6a409c2693429dadb2749db05fca4614aa3e083
Lucas Alves <olalvezluc@gmail.com>
2026-01-16 20:09:26 -0300

    Variável em tempo de de compilação

--- hello.asm
+++ hello.asm
@@ -3,6 +3,8 @@ format ELF64 executable
 SYS_write = 1
 SYS_exite = 60
 
+x = 1
+
 macro write fd, buf, count {
     mov rax, SYS_write
     mov rdi, fd
@@ -17,12 +19,14 @@ macro exit code {
     syscall
 }
 
+x = x + 2
+
 segment readable executable
 entry main
 
 main: 
     
-repeat 5
+repeat x
     write 1, msg, msg_len
 end repeat
 
